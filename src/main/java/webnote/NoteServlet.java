dcb093f17d366c39e6ecfda7585f8163bdb18d7a
Rev <revolnoom7801@gmail.com>
2022-06-22 17:51:36 +0700

    Finish UI

--- src/main/java/webnote/NoteServlet.java
+++ src/main/java/webnote/NoteServlet.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
 
 /**
  * Servlet implementation class NoteServlet
@@ -26,7 +28,7 @@ public class NoteServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append(GetNoteServletContent());
 	}
 
 	/**
@@ -36,5 +38,42 @@ public class NoteServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+	
+	protected String GetNoteServletContent()
+	{
+		return "<html>" + GetStyle() + AddTitleTextBox() + AddNoteContentTextBox() + AddSaveButton() + "</html>";
+	}
 
+	protected String GetStyle()
+	{
+		InputStream style = this.getClass().getResourceAsStream("rss/style.html");
+		String result = new String();
+		try
+		{
+			Scanner s = new Scanner(style);
+			while (s.hasNextLine())
+				result += s.nextLine();
+		}
+		catch (Exception e)
+		{
+			System.out.println("NoteServlet failed to get style");
+		}
+		return result;
+	}
+	
+	protected String AddTitleTextBox()
+	{
+		return "<div><textarea class='note-title-textbox' placeholder='Title'></textarea></div>";
+	}
+
+	protected String AddNoteContentTextBox()
+	{
+		return "<div><textarea class='note-content-textbox' placeholder='Description...'></textarea></div>";
+	}
+	
+	protected String AddSaveButton()
+	{
+		return "<button class='save-button'>SAVE</button>";
+	}
 }
+
